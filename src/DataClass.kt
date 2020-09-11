05ffa2a60e26b0bc13efa65215837fca9e57067f
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2020-09-12 01:43:38 +0300

    Update DataClass.kt

--- src/DataClass.kt
+++ src/DataClass.kt
@@ -8,20 +8,14 @@ fun main() {
 //sampleStart
     val person1 = Person("John")
     val person2 = Person("John")
-    person1.age = 10
-    person2.age = 20
+    
     val people1 = People("John")
     val people2 = People("John")
-    people1.age = 10
-    people2.age = 20
+
 //sampleEnd
     println(message = "person1 == person2: ${person1 == person2}")
-    //println("person1 with age ${person1.age}: ${person1}")
-   // println("person2 with age ${person2.age}: ${person2}")
     println("person1 == person2: ${person1}")
 
     println(message = "people1 == people2: ${people1 == people2}")
-    //println("person1 with age ${person1.age}: ${person1}")
-    // println("person2 with age ${person2.age}: ${person2}")
     println("people1 == people2: ${people1}")
 }
